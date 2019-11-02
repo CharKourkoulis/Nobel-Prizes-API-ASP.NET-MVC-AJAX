fe36428aff4871f0ec98169ccbcd6f2cb0c6318c
babis <babis@hotmail.com>
2019-11-02 22:21:13 +0200

    first commit

---
+++ NobelProjectAPI/Models/NobelDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace NobelProjectAPI.Models
+{
+    public class NobelDBContext : DbContext
+    {
+        public DbSet<Prize> Prizes { get; set; }
+
+
+        public NobelDBContext() : base("name=DBConnection")
+        {
+           //Database.SetInitializer<NobelDBContext>(new DropCreateDatabaseAlways<NobelDBContext>());
+        }
+    }
+}
